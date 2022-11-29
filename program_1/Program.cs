e9f4a1dd08e9860b9d200c9ddc2e26275fdfbb67
Maxim <makfedorovtooo@gmail.com>
2022-11-29 23:56:18 +0300

    Homework is done

---
+++ program_1/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+//a = 5; b = 7 -> max = 7
+//a = 2 b = 10 -> max = 10
+//a = -9 b = -3 -> max = -3
+
+Console.Clear();
+int [] number = new int [2];
+Console.Write("Введите первое число: ");
+number[0] = int.Parse(Console.ReadLine()); 
+Console.Write("Введите второе число: ");
+number[1] = int.Parse(Console.ReadLine()); 
+int max = 0;
+int count = 0;
+while(count<number.Length)
+{
+    if(max<number[count]){ max = number[count];}
+    count++;
+}
+Console.WriteLine($"Максимальное число равно: {max}");
---
+++ program_2/Program.cs
@@ -0,0 +1,17 @@
+﻿//Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+//2, 3, 7 -> 7
+//44 5 78 -> 78
+//22 3 9 -> 22
+
+Console.Clear();
+int [] number = new int [3];
+Console.Write("ВВедите первое число: ");
+number[0] = int.Parse(Console.ReadLine()); 
+Console.Write("Введите второе число: ");
+number[1] = int.Parse(Console.ReadLine());
+Console.Write("Введите третее число: ");
+number[2] = int.Parse(Console.ReadLine()); 
+int max = number[0];
+if(max<number[1]){ max = number[1]; }
+if(max<number[2]){ max = number[2]; }
+Console.WriteLine($"Максимальное число равно {max}");
